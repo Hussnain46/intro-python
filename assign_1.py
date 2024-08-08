0177e3aef883b4bda6dd686301355f7b288b7644
hussnain46 <iamhussnain46@gmail.com>
2024-08-08 10:21:18 +0500

    intro python assignment

---
+++ assign_1.py
@@ -0,0 +1,71 @@
+'''     1) Variable Assignment  '''
+# var_1 = 5
+# var_2 = var_1
+# print(var_2)
+
+
+
+'''     2) Data Types   '''
+# z = 3.5
+# print("The value of z is ", z, "is", type(z).__name__)
+# print(f"The value of z is {z} is {type(z).__name__}")
+
+
+
+'''     3) Type Conversion  '''
+# x = "123"
+# y = int(x)
+# print(y)
+
+
+
+'''     4) Combining Strings    '''
+# str1 = "Hello"
+# str2 = "World"
+# str3 = str1 + " " + str2
+# print(str3)
+
+
+
+'''     5) Basic Arithmetic     '''
+# a = 10
+# b = 20
+# sum = a + b
+# print("Sum of 'a' and 'b' is", sum)
+
+
+
+'''     6) Basic Arithmetic     '''
+# a = 10
+# b = 20
+# product = a * b
+# print("The Product of a and b is", product)
+
+
+
+'''     7) Swapping Variables      '''
+# a = 10
+# b = 20
+
+# a, b = b, a
+# print("After swaping a=",a)
+# print("After swaping b=",b)
+
+
+'''     8) String Repetition    '''
+# str1 = " Hi"
+# print(str1*3)
+
+
+
+'''     9) Float Precision      '''
+# a = 3.14159265
+# print(f"{a:.2f}")
+
+
+
+'''     10) Multiple Assignments    '''
+# a, b, c, = 1, 2, 3
+# print("a = ",a)
+# print("b = ",b)
+# print("c = ",c)
